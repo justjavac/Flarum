20c0a148a12eb87fb7865ced274d1f660cc27789
justjavac <justjavac@gmail.com>
2014-12-24 22:06:36 +0800

    初始化源项目

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+	'installed' => true,
+	'title' => 'Flarum Prototype Forum',
+
+	'route_rules' => array(
+        // 'prefix' => 'blog',
+        // 'domain' => 'blog.site.com'
+    )
+
+);
